af083bc7b405418842176673b55ab0b9241d00f0
Leonel De Vera <leonel.devera@globant.com>
2025-03-11 09:01:28 -0300

    add a step for aws key

--- services-code/aux/app.py
+++ services-code/aux/app.py
@@ -20,10 +20,10 @@ def list_buckets():
         "version": SERVICE_VERSION,
     }
 
-
+# comment
 @app.get("/parameters")
 def list_parameters():
-    params = ssm_client.describe_parameters()#
+    params = ssm_client.describe_parameters()
     return {
         "parameters": [p["Name"] for p in params["Parameters"]],
         "version": SERVICE_VERSION,
